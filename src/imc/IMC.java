1b3b32b664bbd544f19279523c8090321964a452
Jose Gustavo <gustavo.-.josehc@hotmail.com>
2023-11-01 13:44:23 -0500

    Empezando a estudiar algo Java y entender

---
+++ src/imc/IMC.java
@@ -0,0 +1,14 @@
+
+package imc;
+
+
+public class IMC {
+
+    public static void main(String[] args) {
+        
+           IMCventana Main = new IMCventana();
+           Main.setVisible(true);
+
+    }
+    
+}
